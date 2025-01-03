58d542b329eeffb71519ebbe216abcc5fadb7406
Michael Goke <mikhzobby@gmail.com>
2025-01-03 16:13:25 +0100

    stuff

--- backend/messaging/views.py
+++ backend/messaging/views.py
@@ -1,12 +1,8 @@
-from rest_framework import status, serializers, permissions
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import (
     api_view,
     permission_classes
-)
-
-from .models import (
-    Message
 )
 
 from .serializers import (
